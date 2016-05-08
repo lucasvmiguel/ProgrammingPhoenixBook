feb249257a5ba77cb3df50527f4bc43775784273
lucas <lucasvmiguel@live.com>
2016-05-08 16:14:45 -0300

    create user model

--- rumbl/web/controllers/user_controller.ex
+++ rumbl/web/controllers/user_controller.ex
@@ -10,4 +10,21 @@ defmodule Rumbl.UserController do
     user = Repo.get(Rumbl.User, id)
     render conn, "show.html", user: user
   end
+
+  def new(conn, _params) do
+    changeset = Rumbl.User.changeset(%Rumbl.User{})
+    render conn, "new.html", changeset: changeset
+  end
+
+  def create(conn, %{"user" => user_params}) do
+    changeset = Rumbl.User.changeset(%Rumbl.User{}, user_params)
+    case Rumbl.Repo.insert(changeset) do
+      {:ok, user} ->
+        conn
+        |> put_flash(:info, "#{user.name} created!")
+        |> redirect(to: user_path(conn, :index))
+      {:error, changeset} ->
+        render(conn, "new.html", changeset: changeset)
+    end
+  end
 end
