1d4d457f5e5fbd020a457343b365ca81064e72fb
lucas <lucasvmiguel@live.com>
2016-05-12 23:45:15 -0300

    Usuários só tem acesso aos videos do próprio usuário

--- rumbl/web/controllers/video_controller.ex
+++ rumbl/web/controllers/video_controller.ex
@@ -6,17 +6,25 @@ defmodule Rumbl.VideoController do
   plug :scrub_params, "video" when action in [:create, :update]
 
   def index(conn, _params) do
-    videos = Repo.all(Video)
+    videos = Repo.all(user_videos(conn.assigns.current_user))
     render(conn, "index.html", videos: videos)
   end
 
   def new(conn, _params) do
-    changeset = Video.changeset(%Video{})
+    changeset =
+      conn.assigns.current_user
+      |> build_assoc(:videos)
+      |> Video.changeset
+
     render(conn, "new.html", changeset: changeset)
   end
 
   def create(conn, %{"video" => video_params}) do
-    changeset = Video.changeset(%Video{}, video_params)
+    #changeset = Video.changeset(%Video{}, video_params)
+    changeset =
+      conn.assigns.current_user
+      |> build_assoc(:videos)
+      |> Video.changeset(video_params)
 
     case Repo.insert(changeset) do
       {:ok, _video} ->
@@ -29,18 +37,18 @@ defmodule Rumbl.VideoController do
   end
 
   def show(conn, %{"id" => id}) do
-    video = Repo.get!(Video, id)
+    video = Repo.get!(user_videos(conn.assigns.current_user), id) |> Repo.preload(:user)
     render(conn, "show.html", video: video)
   end
 
   def edit(conn, %{"id" => id}) do
-    video = Repo.get!(Video, id)
+    video = Repo.get!(user_videos(conn.assigns.current_user), id)
     changeset = Video.changeset(video)
     render(conn, "edit.html", video: video, changeset: changeset)
   end
 
   def update(conn, %{"id" => id, "video" => video_params}) do
-    video = Repo.get!(Video, id)
+    video = Repo.get!(user_videos(conn.assigns.current_user), id)
     changeset = Video.changeset(video, video_params)
 
     case Repo.update(changeset) do
@@ -54,7 +62,7 @@ defmodule Rumbl.VideoController do
   end
 
   def delete(conn, %{"id" => id}) do
-    video = Repo.get!(Video, id)
+    video = Repo.get!(user_videos(conn.assigns.current_user), id)
 
     # Here we use delete! (with a bang) because we expect
     # it to always work (and if it does not, it will raise).
@@ -63,5 +71,9 @@ defmodule Rumbl.VideoController do
     conn
     |> put_flash(:info, "Video deleted successfully.")
     |> redirect(to: video_path(conn, :index))
+  end
+
+  defp user_videos(user) do
+    Ecto.assoc(user, :videos)
   end
 end
